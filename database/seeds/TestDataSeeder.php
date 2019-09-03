28279f07abdc1e579dadb521d6c0219ef02473f2
uni51 <uni51@users.noreply.github.com>
2019-09-03 17:28:32 +0900

    初期データ投入用のシーダーの準備

--- database/seeds/TestDataSeeder.php
+++ database/seeds/TestDataSeeder.php
@@ -11,6 +11,14 @@ class TestDataSeeder extends Seeder
      */
     public function run()
     {
-        //
+        factory(\App\Customer::class, 2)
+            ->create()
+            ->each(function ($customer) {
+                factory(\App\Report::class, 2)
+                    ->make()
+                    ->each(function ($report) use ($customer) {
+                        $customer->reports()->save($report);
+                    });
+            });
     }
 }
