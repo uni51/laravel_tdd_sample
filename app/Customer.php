239931decf89de7b706a48ddeff7816d4c201498
uni51 <uni51@users.noreply.github.com>
2019-09-03 17:08:06 +0900

    php artisan ide-helper:models -W -R

--- app/Customer.php
+++ app/Customer.php
@@ -4,6 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Customer
+ *
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Report[] $reports
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Customer newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Customer newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Customer query()
+ * @mixin \Eloquent
+ */
 class Customer extends Model
 {
     public function reports()
--- app/Report.php
+++ app/Report.php
@@ -4,6 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Report
+ *
+ * @property-read \App\Customer $customer
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Report newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Report newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Report query()
+ * @mixin \Eloquent
+ */
 class Report extends Model
 {
     public function customer()
